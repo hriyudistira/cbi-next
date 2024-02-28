f31dbb92056e224288872825834f186391f4823f
hriyudistira <hriyudistira@gmail.com>
2024-02-28 13:41:22 +0700

    add dark mode

---
+++ src/app/Providers.jsx
@@ -0,0 +1,15 @@
+'use client'
+import { ThemeProvider } from "next-themes"
+
+
+export default function Providers({children}) {
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+        <div className=" next-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen selection-none transition-colors duration-300">
+            {children}
+        </div>    
+    </ThemeProvider>
+
+  )
+}
+
