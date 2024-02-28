81627cdc52f1918704ad094963f9ed51996cd69c
hriyudistira <hriyudistira@gmail.com>
2024-02-28 14:21:54 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'> 
+        <h1 className='text-2xl text-medium text-amber-600'>About</h1>
+            <p>
+                PT. Century Batteries Indonesia is a battery manufacturer company with INCOE as the main brand. 
+                Founded since 17 September 1971 located at Kawasan Industri Mitra Karawang, West Java – Indonesia.
+            </p>
+
+            <p>
+            As an affiliate company of PT Astra Otoparts, PT Century Batteries Indonesia experienced not only 
+            producing automotive batteries but also for non-automotive categories with the support of manufacturing 
+            facilities and process with international standard.
+            </p>
+            
+            <p>
+            Our quality already recognized worldwide, marked by our products exported to Saudi Arabia in 1992. 
+            Our company production quality has also recognized by domestic automotive industry as 
+            the continued demand to supply OEM (Original Equipment Manufacturing) by many leading automotive
+            manufacturers in Indonesia since year 2011.
+            </p>
+            
+    </div>
+  )
+}
